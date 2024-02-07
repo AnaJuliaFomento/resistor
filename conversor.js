8818867d9d0eed82a8e90a4790c83125a6ced744
Ana julia <ana-julia_f_santos@estudante.sesisenai.org.br>
2024-02-07 14:50:47 -0300

    primeira versao

---
+++ conversor.js
@@ -0,0 +1,12 @@
+
+
+let resistividade = 0.0172 //  0,0172 Ω. mm²/m condutor de cobre 
+
+let comprimento = 1555
+let secaoTrasnversal = 2.5
+let resistencia = 0 
+
+resistencia = ( resistividade * comprimento) / areaTransversal
+
+console.log ("o valor da resitencia é =",resistencia)
+ 
